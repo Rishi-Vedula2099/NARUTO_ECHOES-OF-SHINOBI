653da7b0ef9ca8cc1ac0b251d6f3e6b2bc5c4506
Rishi Vedula <rishivedula93@gmail.com>
2026-09-20 12:53:30 +0530

    docs: add V5 concept documentation, design specifications, and authoring database

---
+++ Source/EchoesOfShinobi/Public/Tests/EOSPhase5Tests.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "../CoreMinimal.h"
+
+/**
+ * Automated Test Suite for Phase 5: Tailed Beast Remnant Power & Jinchūriki System
+ */
+class ECHOESOFSHINOBI_API FEOSPhase5Tests
+{
+public:
+	static bool RunAllPhase5ValidationTests();
+
+	// Phase 5 Test Suites (6 suites)
+	static bool TestTailedBeastRegistrationAndDataAsset();
+	static bool TestTailedBeastCompatibilityRules();
+	static bool TestRemnantPowerChakraContributionMath();
+	static bool TestBeastResonanceStateTransitions();
+	static bool TestTailedBeastTransformationGASBinding();
+	static bool TestTailedBeastTrialRetryPersistence();
+};
